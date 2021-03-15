815e6d2839b4788baf9596d934e7e2129c52d8e4
Jan Peterka <janmpeterka@gmail.com>
2021-03-15 15:20:45 +0100

    Add base browser templates

--- app/controllers/missions.py
+++ app/controllers/missions.py
@@ -2,9 +2,11 @@
 from flask import request
 from flask import render_template as template
 
-
 from flask_classful import FlaskView
+
 from app.models.missions import Mission
+
+from app import fake
 
 
 class MissionView(FlaskView):
@@ -19,6 +21,11 @@ class MissionView(FlaskView):
 
         if subpage == "info":
             content = template("missions/_info.html.j2", mission=mission)
+        elif subpage == "data_browser":
+            content = template(
+                "data_sources/browser/_browser.html.j2", html=fake.text()
+            )
+
         else:
             content = None
 
