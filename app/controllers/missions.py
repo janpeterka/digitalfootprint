5a9444cfd539d43aef056ac419e187f2ea5e01f8
Jan Peterka <janmpeterka@gmail.com>
2021-03-15 17:38:59 +0100

    Add some password guesser data

--- app/controllers/missions.py
+++ app/controllers/missions.py
@@ -1,10 +1,14 @@
-# from flask import redirect, url_for
+import datetime
+
+from flask import url_for, abort
 from flask import request
 from flask import render_template as template
 
 from flask_classful import FlaskView
 
 from app.models.missions import Mission
+from app.models.data_sources.facebook.users import FacebookUser
+from app.models.data_sources.facebook.posts import FacebookPost
 
 from app import fake
 
@@ -17,7 +21,8 @@ class MissionView(FlaskView):
         subpage = request.args.get("page", None)
 
         # mission = Mission.load(id)
-        mission = Mission.random_mission()
+
+        mission = self._load_mission("password_guesser")
 
         if subpage == "info":
             content = template("missions/_info.html.j2", mission=mission)
@@ -25,8 +30,47 @@ class MissionView(FlaskView):
             content = template(
                 "data_sources/browser/_browser.html.j2", html=fake.text()
             )
+        elif subpage == "data_facebook":
+            content = template(
+                "data_sources/facebook/_facebook.html.j2",
+                user=mission.data_sources["facebook"]["user"],
+            )
 
         else:
             content = None
 
         return template("missions/show.html.j2", mission=mission, content=content)
+
+    def _load_mission(self, mission_name):
+        # Password guessing
+        if mission_name == "password_guesser":
+            # Define mission info
+            mission = Mission(
+                id=1,
+                name="Uhodni heslo",
+                info="Uhodni heslo do účtu na sociální sítě na základě veřejně dostupných dat.",
+            )
+
+            # Define data sources
+            mission.data_sources = {}
+            mission.data_sources["facebook"] = {}
+            facebook_user = FacebookUser(full_name="Jakub Ryba")
+            facebook_user.posts = [
+                FacebookPost(
+                    text=f'<img src="{url_for("static", filename="mission_data/password_guesser/photos/birthday_cake.jpg")}"> Nejlepší narozeniny',
+                    created_at=datetime.datetime(2021, 3, 21, 15, 12, 1),
+                ),
+                FacebookPost.random_post(),
+                FacebookPost(
+                    text=f'<img src="{url_for("static", filename="mission_data/password_guesser/photos/dog.jpg")}"> Mám psa!',
+                    created_at=datetime.datetime(2020, 12, 6, 10, 13, 56),
+                ),
+            ]
+            mission.data_sources["facebook"]["user"] = facebook_user
+
+            # Define action pages
+
+        else:
+            abort(404)
+
+        return mission
